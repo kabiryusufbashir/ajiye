c67d22146fde7802be54e4d813dd46c142430700
Piccolo <kabiryusufbashir@gmail.com>
2022-02-02 11:43:54 +0100

    Create login page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,10 +10,38 @@
         <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 
     </head>
-    <body class="bg-green-400">
-        <div class="grid grid-cols-2 gap-4 my-24">
-            <div class="text-white text-5xl">Ajiye</div>
-            <div>Login Form</div>
+    <body class="mx-12">
+        <div class="grid grid-cols-2 gap-4">
+            <!-- Product Logo  -->
+            <div class="text-5xl">
+                <img src="{{ asset('images/ajiye.jpg') }}" alt="Ajiye">
+            </div>
+            <!-- Login Form  -->
+            <div class="w-full flex justify-between leading-snug items-center">
+                <div class="w-full lg:w-2/3 text-center mx-auto shadow-lg">
+                    <div class="bg-green-400 py-4">
+                        <img class="w-12 mx-auto" src="{{ asset('images/cashier.png') }}" alt="Cashier">
+                    </div>
+                    <form action="" method="POST" class="px-6 lg:px-8 py-8">
+                        @csrf
+                        <div>
+                            <input required type="username" name="username" value="{{old('username')}}" placeholder="Username" class="border border-gray-300 rounded py-4 px-6 w-full my-2 focus:outline-none">
+                            @error('username')
+                                {{$message}}
+                            @enderror
+                        </div>
+                        <div>
+                            <input required type="password" name="password" placeholder="Password" class="border border-gray-300 rounded py-4 px-6 w-full my-2 focus:outline-none">
+                            @error('password')
+                                {{$message}}
+                            @enderror
+                        </div>
+                        <div class="text-center mt-6">
+                            <button class="text-xl mx-auto bg-green-400 rounded-full w-full py-3 text-white">LOGIN</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </body>
 </html>
