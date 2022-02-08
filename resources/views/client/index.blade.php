4858f8aaa9ec7dc0eb185ae2e052b41cda7bea39
Piccolo <kabiryusufbashir@gmail.com>
2022-02-08 10:53:56 +0100

    Create trademark

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -10,37 +10,63 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div class="mx-8 my-6">
-        <div class="text-xl mb-10 flex flex-row justify-between">
-            <div>
-                Welcome <b>{{ Auth::user()->staff_username }}</b>
-            </div>
-            <div>
-                Balance: <b>N140,000</b>
-            </div>
-        </div>
-        <div class="grid grid-cols-4 gap-4 text-center text-white text-lg">
-            <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-20 rounded-lg">
+    <div id="container">
+        <!-- Nav  -->
+        <div id="nav" class="bg-green-400 text-center py-3 h-screen">
+            <div class="py-5">
                 <i class="fas fa-book"></i><br>
-                Accounts
             </div>
-            <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-20 rounded-lg">
+            <div class="py-5">
                 <i class="fas fa-file-invoice"></i><br>
-                New Record
             </div>
-            <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-20 rounded-lg">
-                <i class="fas fa-cash-register"></i><br>
-                View Report
+            <div class="py-5">
+                <i class="fas fa-cash-register"></i><br>        
             </div>
-            <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-20 rounded-lg">
+            <div class="py-5">
                 <i class="fas fa-user-circle"></i><br>
-                Staff
             </div>
-            <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-20 rounded-lg">
+            <div class="py-5">
                 <i class="fas fa-sign-out-alt"></i><br>
-                Logout
             </div>
         </div>
-    </div>
+        <!-- Menu  -->
+        <div id="menu" class="my-4">
+            <!-- Notification -->
+            <div class="text-xl mb-10 flex flex-row justify-between border-b py-2">
+                <div>
+                    Welcome <b>{{ Auth::user()->staff_username }}</b>
+                </div>
+                <div>
+                    Balance: <b>N140,000</b>
+                </div>
+            </div>
+            <!-- Section  -->
+            <div class="grid grid-cols-4 gap-4 text-center text-white text-lg">
+                <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-20 rounded-lg">
+                    <i class="fas fa-book"></i><br>
+                    Accounts
+                </div>
+                <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-20 rounded-lg">
+                    <i class="fas fa-file-invoice"></i><br>
+                    New Record
+                </div>
+                <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-20 rounded-lg">
+                    <i class="fas fa-cash-register"></i><br>
+                    View Report
+                </div>
+                <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-20 rounded-lg">
+                    <i class="fas fa-user-circle"></i><br>
+                    Staff
+                </div>
+                <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-20 rounded-lg">
+                    <i class="fas fa-sign-out-alt"></i><br>
+                    Logout
+                </div>
+            </div>
+            <div class="border-b my-6">
+                A Product of Team Piccolo v 1.0
+            </div>
+        </div>
+    </div>  
 </body>
 </html>
